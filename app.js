7d212e7f73a1445db27d736403b57738ee867130
Murat Byazrov <murat.byazrov@yandex.ru>
2020-05-07 11:08:38 +0300

    first commit from master

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+
+// Как можно ещё реализовать обработку подобных ошибок?
+app.get('/:request/:a?', (req, res) => {
+  if (req.params.request !== 'users' || req.params.request !== 'cards') {
+    res.send({ message: 'Запрашиваемый ресурс не найден' });
+  }
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,27 @@
+const usersRouter = require('express').Router();
+const users = require('../data/users.json');
+
+usersRouter.get('/users', (req, res) => {
+  res.send(users);
+});
+
+
+const doesUserExist = ('/users/:id', (req, res, next) => {
+  // eslint-disable-next-line no-underscore-dangle
+  if (!(users.some((item) => item._id === req.params.id))) {
+    res.status(404).send({ message: 'Нет пользователя с таким id' });
+    return;
+  }
+  next();
+});
+
+const sendUser = (req, res) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const user = users.find((item) => item._id === req.params.id);
+  res.send(user);
+};
+
+usersRouter.get('/users/:id', doesUserExist);
+usersRouter.get('/users/:id', sendUser);
+
+module.exports = usersRouter;
