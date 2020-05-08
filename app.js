f1f914127b038b6ff4e859bbef0c6c2d8dcedbab
Murat Byazrov <murat.byazrov@yandex.ru>
2020-05-08 10:57:17 +0300

    after rev1

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
-// Как можно ещё реализовать обработку подобных ошибок? то есть,
-// есть ли стандартный отлавливатель ошибок?)
-app.get('/:request/:a?', (req, res) => {
-  if (req.params.request !== 'users' || req.params.request !== 'cards') {
-    res.send({ message: 'Запрашиваемый ресурс не найден' });
-  }
+app.get('/*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
